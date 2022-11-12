8d425a2a83ef387e4021f65c8eb3bb435c76f434
Ofir Yakovian <ofiryakovian@mail.tau.ac.il>
2022-11-13 00:41:27 +0200

    [Topology]: Added the principal working version of the service, based on a simple inverse-lookup hash table. .

--- src/main/java/com/orca/spring/breacher/controller/AttackSurfaceServiceController.java
+++ src/main/java/com/orca/spring/breacher/controller/AttackSurfaceServiceController.java
@@ -1,18 +1,34 @@
 package com.orca.spring.breacher.controller;
 
+import com.orca.spring.breacher.topology.CloudAssetIdentifier;
+import com.orca.spring.breacher.topology.CloudAssetsTopology;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class AttackSurfaceServiceController
 {
+    // region Dependencies
+
+    @Autowired
+    private CloudAssetsTopology assetsTopology;
+
+    // endregion
+
     @RequestMapping("/attack")
-    public Integer analyzeVirtualMachineAttackSurface()
+    public List<String> analyzeVirtualMachineAttackSurface()
     {
-        return 0;
+        var assetIdentifier = new CloudAssetIdentifier("vm-ab51cba10");
+        var attackSurfaceVms = assetsTopology.analyzeAttackSurface(assetIdentifier);
+
+        return attackSurfaceVms.stream().map(vm -> vm.getVmId()).collect(Collectors.toList());
     }
 
     @RequestMapping("/stats")
