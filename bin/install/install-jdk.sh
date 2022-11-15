2d24003478c6349f1936a7a2e1fd5421239a25ec
Ofir Yakovian <ofiryakovian@mail.tau.ac.il>
2022-11-16 00:39:38 +0200

    [Install]: Place formal shell scripts to ease installation of required packages on Linux

---
+++ bin/install/install-jdk.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+ 
+# Abort if not super user
+if [[ ! `whoami` = "root" ]]; then
+    echo "You must have administrative privileges to run this script"
+    echo "Try 'sudo ./install-jdk.sh <jdk-version> <jdk-archive-name> <target-folder>'"
+    exit 1
+fi
+
+# exit when any command fails
+set -e
+ 
+# Make sure that all Ubuntu OS packages are up to date
+apt-get update
+apt-get upgrade
+
+# Download the JDK to the required destination folder
+wget https://download.oracle.com/java/"$1"/latest/"$2" -P "$3"
+
+# Affirm completion and exit
+echo "Java Development Kit [version $1] was successfully downloaded."
+exit 0
